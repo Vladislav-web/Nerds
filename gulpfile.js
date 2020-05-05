1e51519d6a3f7ce9b8a216e31d46631f26680854
Vladislav-web <ykt.pc_master@mail.ru>
2020-05-05 11:49:08 +0300

    все круто

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('sass', function () {
 });
 
 gulp.task('html', function () {
-	return gulp.src('.html')
+	return gulp.src('*.html')
 		.pipe(browserSync.reload({ stream: true }))
 });
 
@@ -32,7 +32,7 @@ gulp.task('browser-sync', function () {
 
 gulp.task('watch', function () {
 	gulp.watch('src/sass/**/*.sass', gulp.parallel('sass'))
-	gulp.watch('.html', gulp.parallel('html'))
+	gulp.watch('*.html', gulp.parallel('html'))
 	gulp.watch('src/js/*.js', gulp.parallel('script'))
 });
 
