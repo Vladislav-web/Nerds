5f95ec094c3ed42dd9a6c3507b6bc5e5be4ceb93
Vladislav-web <ykt.pc_master@mail.ru>
2020-05-05 11:19:12 +0300

    новая эра

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+let gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename');
+
+gulp.task('sass', function () {
+	return gulp.src('src/sass/**/*.sass')
+		.pipe(sass({ outputStyle: 'compressed' }))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('html', function () {
+	return gulp.src('.html')
+		.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('script', function () {
+	return gulp.src('src/js/*.js')
+		.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('browser-sync', function () {
+	browserSync.init({
+		server: {
+		},
+		notify: false
+	});
+});
+
+gulp.task('watch', function () {
+	gulp.watch('src/sass/**/*.sass', gulp.parallel('sass'))
+	gulp.watch('.html', gulp.parallel('html'))
+	gulp.watch('src/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'))
+
---
+++ src/js/main.js
@@ -0,0 +1,55 @@
+btn.addEventListener('click', () => { pop.classList.add('active') });
+cls.addEventListener('click', () => { pop.classList.remove('active') });
+
+// let button = document.querySelector('.button1');
+// let pop = document.querySelector('.pop');
+// let close = document.querySelector('.close');
+
+// button.addEventListener('click', function () {
+// 	pop.classList.add('active');
+// });
+
+// close.addEventListener('click', function () {  
+// 	pop.classList.remove('active');
+// })
+
+// document.querySelector('#one').oninput = () => {
+// 	console.log(document.querySelector('#one').value);
+// 	document.querySelector('span').innerHTML = document.querySelector('#one').value
+// }
+
+// document.querySelector('#btn-1').onclick = () => {
+// 	console.log(document.querySelector('#i2').value);
+// 	let myCheckbox = document.querySelector('#i2');
+
+// ymaps.ready(init);
+// function init() {
+// 	let map = new ymaps.Map("map", {
+// 		center: [59.938631, 30.321075],
+// 		zoom: 17.2,
+// 		controls: ['zoomControl'],
+// 		behaviors: ['drag']
+// 	});
+
+// 	let placemark = new ymaps.Placemark([59.938631, 30.323055], {
+// 		hintContent: 'Nerds',
+// 		balloonContent: 'Nerds'
+// 	});
+// 	map.geoObjects.add(placemark);
+// }
+
+
+// // создаем переменную и находим селектор в документе
+// const button = document.querySelector('.basket1');
+// const close = document.querySelector('.close');
+
+// // переменному добавляем событие при клике 
+// button.addEventListener('click', function () {
+// 	// добавляем класс, при добавлении класса отрывается модалка
+// 	pop.classList.add('active');
+// });
+// // переменному добавляем событие при клике
+// close.addEventListener('click', function () {
+// 	// удаляем класс, при удалении класса закрывается модальное окно 
+// 	pop.classList.remove('active')
+// })
